f45938b992bd8ed70973bc044f5dd56208e390d3
tomas olivari <tomas_olivari96@hotmail.com>
2023-11-02 21:43:08 -0300

    pre entrega 3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 let cumpleRequisitos = false;
+const body = document.getElementsByTagName("body")
+const containerPartidos = document.getElementById("evento-container")
+const partidosList = document.getElementById('partidos-list');
+const divGanancias = document.getElementById('calculo-ganancias');
 
 //llamo a los inputs del html
 const nombreInput = document.getElementById("nombre-input");
@@ -7,7 +11,8 @@ const paisInput = document.getElementById("pais-input");
 const enviarButton = document.getElementById("enviar-button");
 
 //ingreso los datos
-enviarButton.addEventListener("click", function () {
+enviarButton.addEventListener("click", function (event) {
+  event.preventDefault()
   const nombre = nombreInput.value.trim().toLowerCase();
   const edad = parseInt(edadInput.value);
   const pais = paisInput.value.trim().toLowerCase();
@@ -26,6 +31,10 @@ enviarButton.addEventListener("click", function () {
   } else if (edad >= 18 && pais === "argentina") {
     alert("Muchas gracias, ya puedes disfrutar de nuestro sitio de apuestas.");
     cumpleRequisitos = true;
+    mostrarPartidos();
+    nombreInput.value = '';
+    edadInput.value = '';
+    paisInput.value = '';
   }
 
   // Almacena la información en el localStorage
@@ -53,60 +62,96 @@ localStorage.setItem("apuestas disponibles", apuestasJSON)
 
 
 function mostrarPartidos() {
-  const partidosList = document.getElementById('partidos-list');
+  const titulo = document.createElement('h1')
+  titulo.innerHTML = "Eventos Disponibles"
+  const subTitulo = document.createElement('h2')
+  subTitulo.innerHTML = "Roland Garros - Octavos De Final"
+  partidosList.append(titulo, subTitulo)
 
   apuestasDisponibles.forEach((apuesta, indice) => {
     const partidoItem = document.createElement('li');
     partidoItem.textContent = `${indice}. ${apuesta.jugador1} vs ${apuesta.jugador2}`;
     partidosList.appendChild(partidoItem);
   });
+
+  partidoAelegir()
 }
 
-mostrarPartidos();
+function partidoAelegir() {
+  const parrafo = document.createElement("p")
+  parrafo.innerHTML = "Selecciona el número del partido en el que deseas apostar:"
+  const inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "number"); // Establece el tipo de entrada como número
+  inputElement.setAttribute("id", "indice-partido"); // Establece el ID del input
+  inputElement.setAttribute("placeholder", "Índice del partido");
+  const apostarBtn = document.createElement("button")
+  apostarBtn.innerHTML = "Ver Partido"
+  apostarBtn.setAttribute("id", "apostarBtn")
+  partidosList.append(parrafo, inputElement, apostarBtn)
 
+  document.getElementById('apostarBtn').addEventListener('click', function () {
+    const indicePartido = parseInt(document.getElementById('indice-partido').value);
 
-document.getElementById('apostar-btn').addEventListener('click', function () {
-  const indicePartido = parseInt(document.getElementById('indice-partido').value);
+    if (isNaN(indicePartido) || indicePartido < 0 || indicePartido >= apuestasDisponibles.length) {
+      alert('Ingresa un índice válido.');
+      return;
+    }
 
-  if (isNaN(indicePartido) || indicePartido < 0 || indicePartido >= apuestasDisponibles.length) {
-    alert('Ingresa un índice válido.');
-    return;
-  }
-
-  const apuestaSeleccionada = apuestasDisponibles[indicePartido];
-  const apuestaContainer = document.getElementById('apuesta-container');
-
-  
-  const apuestaCard = document.createElement('div');
-  apuestaCard.classList.add('apuesta-card');
-  apuestaCard.innerHTML = `
-      <h2>${apuestaSeleccionada.jugador1} vs ${apuestaSeleccionada.jugador2}</h2>
-      <button id="ganancia1-btn">${apuestaSeleccionada.ganancia1}</button>
-      <button id="ganancia2-btn">${apuestaSeleccionada.ganancia2}</button>
-    `;
-
-  // Agrego un evento click a los botones de ganancia
-  const ganancia1Btn = apuestaCard.querySelector('#ganancia1-btn');
-  const ganancia2Btn = apuestaCard.querySelector('#ganancia2-btn');
-
-  ganancia1Btn.addEventListener('click', function () {
-    // Dirige el foco al campo de entrada al input cantidad-apuesta
-    document.getElementById('cantidad-apuesta').focus();
-  });
-
-  ganancia2Btn.addEventListener('click', function () {
     
-    document.getElementById('cantidad-apuesta').focus();
+
+    const apuestaSeleccionada = apuestasDisponibles[indicePartido];
+    const apuestaContainer = document.getElementById('apuesta-container');
+
+
+    const card = document.createElement("div")
+    card.classList.add("card")
+
+    const jugador = document.createElement("p")
+    jugador.innerHTML = `<strong>Jugadores:</strong>  ${apuestaSeleccionada.jugador1} vs ${apuestaSeleccionada.jugador2}`
+    card.appendChild(jugador)
+
+    const ganancia = document.createElement("p")
+    ganancia.innerHTML = `<strong> Ganancias: </strong> <button id="boton1">${apuestaSeleccionada.ganancia1}</button> - <button id="boton2">${apuestaSeleccionada.ganancia2}</button>`
+    card.appendChild(ganancia)
+    apuestaContainer.appendChild(card);
+
+
+    // Agrego un evento click a los botones de ganancia
+    const boton1 = document.getElementById("boton1");
+    const boton2 = document.getElementById("boton2");
+
+    boton1.addEventListener('click', function () {
+      // Dirige el foco al campo de entrada al input cantidad-apuesta
+      document.getElementById('cantidadApuesta').focus();
+    });
+
+    boton2.addEventListener('click', function () {
+
+      document.getElementById('cantidadApuesta').focus();
+    });
+
   });
 
-  apuestaContainer.innerHTML = ''; // Limpia cualquier tarjeta anterior
-  apuestaContainer.appendChild(apuestaCard);
-});
+  calcularGanancia()
+}
 
+function calcularGanancia() {
 
-document.getElementById('calcular-ganancia-btn').addEventListener('click', function () {
+  const pGanancia = document.createElement("p")
+  pGanancia.innerHTML = "Ingresa un monto para calcular las ganancias:"
+  const inputGanancias = document.createElement("input");
+  inputGanancias.setAttribute("type", "number"); // Establece el tipo de entrada como número
+  inputGanancias.setAttribute("id", "cantidadApuesta"); // Establece el ID del input
+  inputGanancias.setAttribute("placeholder", "Cantidad de apuesta");
+  const btnGanancias = document.createElement("button")
+  btnGanancias.innerHTML = "Calcular Ganancia"
+  btnGanancias.setAttribute("id", "calcularGananciaBtn")
+
+  divGanancias.append(pGanancia, inputGanancias, btnGanancias)
+
+ document.getElementById('calcularGananciaBtn').addEventListener('click', function () {
   const indicePartido = parseInt(document.getElementById('indice-partido').value);
-  const cantidadApuesta = parseFloat(document.getElementById('cantidad-apuesta').value);
+  const cantidadApuesta = parseFloat(document.getElementById('cantidadApuesta').value);
 
   if (isNaN(indicePartido) || indicePartido < 0 || indicePartido >= apuestasDisponibles.length) {
     alert('Ingresa un índice de partido válido.');
@@ -118,6 +163,8 @@ document.getElementById('calcular-ganancia-btn').addEventListener('click', funct
     return;
   }
 
+  inputGanancias.value = '';
+
   const apuestaSeleccionada = apuestasDisponibles[indicePartido];
   const ganancia1 = apuestaSeleccionada.ganancia1;
   const ganancia2 = apuestaSeleccionada.ganancia2;
@@ -128,6 +175,8 @@ document.getElementById('calcular-ganancia-btn').addEventListener('click', funct
   alert(`Si apuestas $${cantidadApuesta} en ${apuestaSeleccionada.jugador1}, tus ganancias serán $${gananciaJugador1}`);
   alert(`Si apuestas $${cantidadApuesta} en ${apuestaSeleccionada.jugador2}, tus ganancias serán $${gananciaJugador2}`);
 });
+
+}
 
 
 const nombreAlmacenado = localStorage.getItem("nombre");
@@ -141,7 +190,7 @@ if (nombreAlmacenado && edadAlmacenada && paisAlmacenado) {
   const edad = parseInt(edadAlmacenada);
   const pais = paisAlmacenado;
 
-  
+
   console.log("Nombre:", nombre);
   console.log("Edad:", edad);
   console.log("Pais:", pais);
